817649ebb004b12a4a8f89a956dd6952f6965b54
Gridenko Maks <Maksgridenko@ya.ru>
2023-12-22 20:21:26 +0300

    Initial Commit

---
+++ SliderForUIKit/SliderForUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderForUIKitApp.swift
+//  SliderForUIKit
+//
+//  Created by Maxim Gridenko on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderForUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
